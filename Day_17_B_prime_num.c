7d5cdc5971df6f4716b597b1560a26f58e8eaaf1
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-09-27 19:22:43 +0530

    Create Day_17_B_prime_num.c

---
+++ Day_17_B_prime_num.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int num, i;
+    bool isPrime = true;
+
+    printf("Enter an integer greater than 1: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("%d is not a prime number.\n", num);
+        return 0;
+    }
+
+    for (i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
