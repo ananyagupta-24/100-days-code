079ce183488ce2e4cb05a03137745279a2762e16
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-09-27 18:50:40 +0530

    Create Day_12_A_libraryfine.c

---
+++ Day_12_A_libraryfine.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int daysLate;
+    int fine = 0;
+
+    printf("Enter number of late days: ");
+    scanf("%d", &daysLate);
+
+    if (daysLate <= 0) {
+        printf("No fine. Book returned on time.\n");
+    } else if (daysLate <= 5) {
+        fine = daysLate * 2;
+        printf("Fine = ₹%d\n", fine);
+    } else if (daysLate <= 10) {
+        fine = (5 * 2) + ((daysLate - 5) * 4);
+        printf("Fine = ₹%d\n", fine);
+    } else if (daysLate <= 30) {
+        fine = (5 * 2) + (5 * 4) + ((daysLate - 10) * 6);
+        printf("Fine = ₹%d\n", fine);
+    } else {
+        printf("Fine = ₹0\n");
+        printf("Membership Cancelled due to delay of more than 30 days.\n");
+    }
+
+    return 0;
+}
