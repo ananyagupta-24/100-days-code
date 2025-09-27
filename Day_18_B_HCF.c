f37b6ab8f5d98422b5f6e182903f9b0ce43e549c
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-09-27 19:25:51 +0530

    Create Day_18_B_HCF.c

---
+++ Day_18_B_HCF.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+   
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("HCF (GCD) of the two numbers is: %d\n", a);
+
+    return 0;
+}
