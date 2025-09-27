30086a7abddc7d745c4843e6bb23cb08a5a68255
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-09-27 19:33:43 +0530

    Create Day_20_A_product_of_odd_digits.c

---
+++ Day_20_A_product_of_odd_digits.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int num, remainder;
+    long long product = 1;  
+    int hasOdd = 0;        
+
+   
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    
+    if (num < 0) {
+        num = -num;
+    }
+
+    while (num != 0) {
+        remainder = num % 10;
+        if (remainder % 2 != 0) {  
+            product *= remainder;
+            hasOdd = 1;
+        }
+        num /= 10;
+    }
+
+    if (hasOdd)
+        printf("Product of odd digits: %lld\n", product);
+    else
+        printf("No odd digits found in the number.\n");
+
+    return 0;
+}
