d42ae7d4712de071cb874e97ac5bce7c0eb80701
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-09-27 19:27:19 +0530

    Create Day_19_A_LCM.c

---
+++ Day_19_A_LCM.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a, b, lcm_value;
+
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+
+    int gcd_value = gcd(a, b);
+    lcm_value = (a / gcd_value) * b;  
+
+    printf("LCM of %d and %d is: %d\n", a, b, lcm_value);
+
+    return 0;
+}
