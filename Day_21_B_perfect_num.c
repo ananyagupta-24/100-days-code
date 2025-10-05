061c23f98d39c461c85d1fb4e4e228978a7f663c
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-10-05 15:56:10 +0530

    Create Day_21_B_perfect_num.c

---
+++ Day_21_B_perfect_num.c
@@ -0,0 +1,29 @@
+// Write a program to check if a number is a perfect number
+
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num) {
+        printf("%d is a perfect number.\n", num);
+    } else {
+        printf("%d is not a perfect number.\n", num);
+    }
+
+    return 0;
+}
