969bfb56622271eea8c35e9652179567fa5a7863
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-10-05 16:09:26 +0530

    Create Day_24_A_patterns2.c

---
+++ Day_24_A_patterns2.c
@@ -0,0 +1,24 @@
+//Write a program to print the following pattern:
+//*
+//**
+//***
+//****
+//*****
+
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        // Print i stars in each row
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
+
+
