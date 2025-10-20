6f11b735953b8578a4c9edb0ba87ea1930087ee8
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-10-20 21:54:15 +0530

    Create Day_32_A_merging_an_array.c

---
+++ Day_32_A_merging_an_array.c
@@ -0,0 +1,35 @@
+// merge an array
+#include <stdio.h>
+
+void mergeArrays(int arr1[], int size1, int arr2[], int size2, int merged[]) {
+    for (int i = 0; i < size1; i++) {
+        merged[i] = arr1[i];
+    }
+
+    for (int i = 0; i < size2; i++) {
+        merged[size1 + i] = arr2[i];
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr1[] = {1, 3, 5};
+    int arr2[] = {2, 4, 6};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    int merged[size1 + size2]; 
+
+    mergeArrays(arr1, size1, arr2, size2, merged);
+
+    printf("Merged array: ");
+    printArray(merged, size1 + size2);
+
+    return 0;
+}
