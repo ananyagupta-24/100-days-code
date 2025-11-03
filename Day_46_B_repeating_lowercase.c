d6d13ec54a9b2b0e112297b8bd263c615ef37fa1
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-11-03 15:53:13 +0530

    Create Day_46_B_repeating_lowercase.c

---
+++ Day_46_B_repeating_lowercase.c
@@ -0,0 +1,32 @@
+//Find the first repeating lowercase alphabet in a string.
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int freq[26] = {0}; 
+    int i;
+    char result = '\0';  
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  
+
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        if (ch >= 'a' && ch <= 'z') {
+            freq[ch - 'a']++;
+
+            if (freq[ch - 'a'] == 2) {
+                result = ch;
+                break;
+            }
+        }
+    }
+
+    if (result != '\0')
+        printf("First repeating lowercase alphabet: %c\n", result);
+    else
+        printf("No repeating lowercase alphabet found.\n");
+
+    return 0;
+}
