c548d9997abd19664f34bb3e7a7d2f2be64a9eef
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-11-03 16:12:31 +0530

    Create Day_47_B_finnding_lomngest_world.c

---
+++ Day_47_B_finnding_lomngest_world.c
@@ -0,0 +1,37 @@
+//Find the longest word in a sentence.
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+    char word[50], longest[50];
+    int i = 0, j = 0, maxLen = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);  
+    while (str[i] != '\0') {
+        if (!isspace(str[i]) && str[i] != '\n') {
+            word[j++] = str[i];
+        } else {
+            word[j] = '\0';
+            if (j > maxLen) {
+                maxLen = j;
+                strcpy(longest, word);
+            }
+            j = 0;
+        }
+        i++;
+    }
+
+    word[j] = '\0';
+    if (j > maxLen) {
+        maxLen = j;
+        strcpy(longest, word);
+    }
+
+    printf("The longest word: %s\n", longest);
+    printf("Length: %d\n", maxLen);
+
+    return 0;
+}
