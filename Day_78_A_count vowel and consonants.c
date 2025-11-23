4a001d592e58abb886a9de407003a6e1e75efd34
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-11-23 19:23:18 +0530

    Create Day_78_A_count vowel and consonants.c

---
+++ Day_78_A_count vowel and consonants.c
@@ -0,0 +1,35 @@
+//Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    int ch, vowels = 0, consonants = 0;
+    char filename[100];
+
+    printf("Enter filename: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (isalpha(ch)) {
+            ch = tolower(ch);
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    fclose(fp);
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
