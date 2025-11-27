92553f3791afc95479c1aec8f8c5a259fab8234b
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-11-27 22:34:06 +0530

    Create Day_81_A_enum for days.c

---
+++ Day_81_A_enum for days.c
@@ -0,0 +1,28 @@
+//Create an enumeration for days (SUNDAY to SATURDAY) and print each day with its integer value.
+#include <stdio.h>
+
+enum Day {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+};
+
+int main() {
+    enum Day d;
+
+    for (d = SUNDAY; d <= SATURDAY; d++) {
+        if (d == SUNDAY) printf("SUNDAY = %d\n", d);
+        if (d == MONDAY) printf("MONDAY = %d\n", d);
+        if (d == TUESDAY) printf("TUESDAY = %d\n", d);
+        if (d == WEDNESDAY) printf("WEDNESDAY = %d\n", d);
+        if (d == THURSDAY) printf("THURSDAY = %d\n", d);
+        if (d == FRIDAY) printf("FRIDAY = %d\n", d);
+        if (d == SATURDAY) printf("SATURDAY = %d\n", d);
+    }
+
+    return 0;
+}
