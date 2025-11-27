b9f913c531f7b1d2c785dfdc34c9cdc8c9a2e269
ananyagupta-24 <ananyamsgupta1234@gmail.com>
2025-11-27 23:03:20 +0530

    Create Day_88_A_ enum withloop.c

---
+++ Day_88_A_ enum withloop.c
@@ -0,0 +1,22 @@
+//Print all enum names and integer values using a loop.
+#include <stdio.h>
+
+enum Role {
+    ADMIN,
+    USER,
+    GUEST
+};
+
+int main() {
+    enum Role r;
+
+    char *roleNames[] = {"ADMIN", "USER", "GUEST"};
+
+    for (r = ADMIN; r <= GUEST; r++) {
+        printf("%s = %d\n", roleNames[r], r);
+    }
+
+    return 0;
+}
+
+
